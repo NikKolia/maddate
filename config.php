40b3ba5e70aa2dfe38900473f34466dfc47ebffc
Roman <nikkolia7777@gmail.com>
2020-01-23 10:18:41 +0200

    original working

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "maddate.loc";
+
+$dbusername = "root";
+
+$dbpassword = "1111";
+
+$dbname = "romanm12_dating";
+
+
+$site_url = "http://maddate.loc"; 
+
+$app = "QuickDating";
+$endpoint_v = "v1";
+$serverkey = "9845b420acca9a2740b10f941b27ce69";
+
+
+?>
